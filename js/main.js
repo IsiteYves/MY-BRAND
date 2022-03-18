019e71969071db7a47cc62a94c594570fea06a9f
IsiteYves <yvesisite@gmail.com>
2022-03-18 11:30:02 +0200

    Merge branch 'master' of https://github.com/IsiteYves/MY-BRAND

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-const menuBlockLink = document.querySelector("#menu-block-link");
-menuBlockLink.onclick = () => {
-  const { host } = window.location;
-  window.location = host;
+// const menuBlockLink = document.querySelector("#menu-block-link");
+// menuBlockLink.onclick = () => {
+//   const { host } = window.location;
+//   window.location = host;
+// };
+const menuIcon = document.querySelector("#menuIcon"),
+  menubarRemoveIcon = document.getElementById("menu-block-link");
+menuIcon.addEventListener("click", () => {
+  document.getElementsByClassName("Menubar")[0].style.display = "block";
+});
+menubarRemoveIcon.onclick = () => {
+  document.getElementsByClassName("Menubar")[0].style.display = "none";
 };
