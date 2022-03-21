39591c2e8a9c390eae71b6c2f49c6929acafd3c3
IsiteYves <yvesisite@gmail.com>
2022-03-21 21:30:33 +0200

    Added the uncontrolled login

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-// const menuBlockLink = document.querySelector("#menu-block-link");
-// menuBlockLink.onclick = () => {
-//   const { host } = window.location;
-//   window.location = host;
-// };
-const menuIcon = document.querySelector("#menuIcon"),
-  menubarRemoveIcon = document.getElementById("menu-block-link");
-menuIcon.addEventListener("click", () => {
-  document.getElementsByClassName("Menubar")[0].style.display = "block";
-});
-menubarRemoveIcon.onclick = () => {
-  document.getElementsByClassName("Menubar")[0].style.display = "none";
+if (document.getElementsByClassName("menu-block-link")[0]) {
+  const menuBlockLink = document.getElementsByClassName("menu-block-link")[0];
+  menuBlockLink.onclick = (e) => {
+    // console.log(e.target.id);
+    const isCloseClicked = e.target.id == "menubarRemoveIcon";
+    if (isCloseClicked) return;
+    if (!isCloseClicked) {
+      document.location.href = "/";
+    }
+  };
+}
+if (document.querySelector("#menuIcon")) {
+  const menuIcon = document.querySelector("#menuIcon");
+  menuIcon.addEventListener("click", () => {
+    document.getElementsByClassName("Menubar")[0].style.display = "block";
+  });
+}
+if (document.getElementById("menubarRemoveIcon")) {
+  menubarRemoveIcon = document.getElementById("menubarRemoveIcon");
+  menubarRemoveIcon.onclick = () => {
+    document.getElementsByClassName("Menubar")[0].style.display = "none";
+  };
+}
+const handleSubmit = () => {
+  console.log("submitted");
+  document.location.href = "/ui/admin-dashboard.html";
 };
-// alert(document.documentElement.clientWidth);
