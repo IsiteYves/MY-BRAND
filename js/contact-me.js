db3ed05957a920ac20131161ae2abc00f3b103d2
IsiteYves <yvesisite@gmail.com>
2022-04-01 18:47:28 -0700

    Completed signup validation

---
+++ js/contact-me.js
@@ -0,0 +1,57 @@
+function getUserInfoFromLocalStorage() {
+  let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  if (userInfo) document.location.href = "/ui/admin-dashboard.html";
+}
+const handleSubmit = () => {
+  let emailField = document.getElementById("email"),
+    namesField = document.getElementById("names"),
+    messageField = document.getElementById("message"),
+    errorParagraph = document.getElementById("error-paragraph"),
+    errorsFound = false,
+    success = false;
+  if (emailField.value == "") {
+    errorParagraph.innerHTML = "Email can not be empty.";
+    errorsFound = true;
+  } else {
+    if (namesField.value == "") {
+      errorParagraph.innerHTML = "Names can't be empty.";
+      errorsFound = false;
+    } else {
+      if (namesField.value.length < 3) {
+        errorParagraph.innerHTML = "Names must be at least 3 characters long.";
+        errorsFound = false;
+      } else {
+        if (messageField.value == "") {
+          errorParagraph.innerHTML = "Message can't be empty.";
+          errorsFound = true;
+        } else {
+          if (messageField.value.length < 3) {
+            errorParagraph.innerHTML =
+              "Message has to be at least 3 characters long.";
+            errorsFound = true;
+          } else {
+            success = true;
+            const messageObj = {
+              senderEmail: emailField.value,
+              senderNames: namesField.value,
+              messageText: messageField.value,
+            };
+          }
+        }
+      }
+    }
+  }
+  if (errorsFound || success) {
+    emailField.classList.toggle("move-upper");
+  }
+  if (errorsFound) {
+    errorParagraph.style.display = "inline";
+  }
+  if (success) {
+    errorParagraph.style.display = "none";
+    successSpan.style.display = "inline";
+  }
+};
+window.onload = () => {
+  getUserInfoFromLocalStorage();
+};
