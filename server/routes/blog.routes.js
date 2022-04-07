cbc86963e8e1848e2370e492f84b14866e171cf3
IsiteYves <yvesisite@gmail.com>
2022-04-06 19:57:39 -0700

    [fix] moved the backend aside

--- server/routes/blog.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const {createNewBlog, getAllBlogs, updateBlog, deleteBlog}=require("../controllers/blog")
-const router = express.Router()
-
-router.post("/", createNewBlog)
-router.get("/", getAllBlogs)
-router.patch("/:id", updateBlog)
-router.delete("/:id", deleteBlog)
--- server/utils/hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcrypt");
-
-async function hashData(data) {
-  let salt = await bcrypt.genSalt(10);
-  let hashed = await bcrypt.hash(data, salt);
-  return hashed;
-}
-
-module.exports = hashData;
